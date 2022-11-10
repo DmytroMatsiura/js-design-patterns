aa03f5af1b2fc78c5d8b24a49ad2393c0bcd12f8
Dmytro Matsiura <dmytro.matsiura@gotoinc.co>
2022-11-11 00:23:54 +0200

    add creational patterns

---
+++ creational/prototype.js
@@ -0,0 +1,33 @@
+function CustomerPrototype(proto) {
+  this.proto = proto
+
+  this.clone = function () {
+    const customer = new Customer()
+
+    customer.first = proto.first
+    customer.last = proto.last
+    customer.status = proto.status
+
+    return customer
+  }
+}
+
+function Customer(first, last, status) {
+
+  this.first = first
+  this.last = last
+  this.status = status
+
+  this.say = function () {
+      console.log(`Name: ${this.first} ${this.last}, status: ${this.status}`)
+  }
+}
+
+function run() {
+
+  const proto = new Customer('n/a', 'n/a', 'pending')
+  const prototype = new CustomerPrototype(proto)
+
+  const customer = prototype.clone()
+  customer.say()
+}
